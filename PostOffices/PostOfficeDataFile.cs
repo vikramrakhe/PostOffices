23401fe681c940a682bc14fcc5a0977b2a2a7b42
Geoff Bones <geoff.bones@siemens.com>
2016-03-22 12:15:24 +0000

    green: First implementation
    really ugly

--- PostOffices/PostOfficeDataFile.cs
+++ PostOffices/PostOfficeDataFile.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace PostOffices
 {
@@ -6,7 +10,13 @@ namespace PostOffices
     {
         public IEnumerable<PostOffice> Read()
         {
-            return new List<PostOffice>();
+            const string fileName = "all_india_PO_list_without_APS_offices_ver2.csv";
+            var directory = Assembly.GetExecutingAssembly().CodeBase;
+            var x = new Uri(directory);
+            var xx = Path.GetDirectoryName(x.AbsolutePath);
+            var filePath = Path.Combine(xx, fileName);
+            var lines = File.ReadAllLines(filePath);
+            return lines.Select(line => new PostOffice());
         }
     }
 }
