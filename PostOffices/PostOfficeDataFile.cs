21c269cdc228bf5e546c83b4f8720dcad3d59274
Geoff Bones <geoff.bones@siemens.com>
2016-03-22 11:57:59 +0000

    Red: Add failing test for reading the data file

---
+++ PostOffices/PostOfficeDataFile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PostOffices
+{
+    public class PostOfficeDataFile
+    {
+        public IEnumerable<PostOffice> Read()
+        {
+            return new List<PostOffice>();
+        }
+    }
+}
---
+++ Tests/PostOfficeDataFileTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using PostOffices;
+
+namespace Tests
+{
+    public class PostOfficeDataFileTests
+    {
+        [Test]
+        public void GivenPostOfficeDataFile_WhenReadCalled_ShouldReadDataOnOver1500000PostOffices()
+        {
+            var file = new PostOfficeDataFile();
+            var postOffices = file.Read();
+            Assert.That(postOffices.Count(), Is.GreaterThan(150000));
+        }
+    }
+}
