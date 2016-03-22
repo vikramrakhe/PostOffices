c0972faac541e5eace2a78cb03511aa3adf30aaa
Geoff Bones <geoff.bones@siemens.com>
2016-03-22 16:44:02 +0000

    green: implement add and find

--- Tests/DictionaryNode.cs
+++ Tests/DictionaryNode.cs
@@ -1,15 +1,35 @@
+using System.Collections.Generic;
+
 namespace Tests
 {
     public class DictionaryNode
     {
+        private readonly Dictionary<char, DictionaryNode> m_Nodes = new Dictionary<char, DictionaryNode>();
         public void Add(string word)
         {
-            
+            var currentNode = this;
+            foreach (var c in word)
+            {
+                DictionaryNode childNode;
+                if (!currentNode.m_Nodes.TryGetValue(c, out childNode))
+                {
+                    childNode = new DictionaryNode();
+                    currentNode.m_Nodes.Add(c, childNode);
+                }
+                currentNode = childNode;
+            }
         }
 
         public DictionaryNode Find(string word)
         {
-            return new DictionaryNode();
+            var currentNode = this;
+            foreach (var c in word)
+            {
+                DictionaryNode childNode;
+                if (!currentNode.m_Nodes.TryGetValue(c, out childNode)) return null;
+                currentNode = childNode;
+            }
+            return currentNode;
         }
     }
 }
