0acfb3ea5b92b1be1796e16ac1caae80b4d644ee
Geoff Bones <geoff.bones@siemens.com>
2016-03-22 14:54:16 +0000

    Add tset data

--- Tests/PostOfficeNameCompletionTests.cs
+++ Tests/PostOfficeNameCompletionTests.cs
@@ -6,10 +6,16 @@ namespace Tests
 {
     public class PostOfficeNameCompletionTests
     {
+
+       private static string s_AchalPur_MP = "Achalpur B.O";
+       private static string s_AchalPur_RJ = "Achalpur B.O";
+       private static string s_AchalPurCity = "Achalpur City S.O";
+       private static string s_Achalu = "Achalu";
+ 
         [Test]
         public void WhenCompletionCalledWithEmptyString_ShouldReturnEmptySet()
         {
-            var testData = new List<string>();
+            var testData = new List<string>{ s_AchalPur_MP, s_AchalPur_RJ, s_AchalPurCity, s_Achalu };
             var completer = new PostOfficeNameCompleter(testData);
             var completions = completer.SuggestCompletedNames(string.Empty);
             Assert.That(completions, Is.EquivalentTo(new List<string>()));
