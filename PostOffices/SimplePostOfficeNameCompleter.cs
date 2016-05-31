0040796b099d02b6eca625fe0df44884d4d422e4
Vikram Rakhe <vikram.rakhe@siemens.com>
2016-05-31 16:10:49 +0530

    PostOffices Red: Failing test case for SimplePostOfficeNameCompleter (LINQ Based Class)

---
+++ PostOffices/SimplePostOfficeNameCompleter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostOffices
+{
+    public class SimplePostOfficeNameCompleter
+    {
+        public IEnumerable<string> SuggestCompletedNames(string startOfName)
+        {
+            return new List<string>().DefaultIfEmpty();
+        }
+    }
+}
---
+++ Tests/SimplePostOfficeNameCompleterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using PostOffices;
+
+namespace Tests
+{
+    public class SimplePostOfficeNameCompleterTest
+    {
+        [Test]
+        public void GivenPostOfficeName_WhenSimplePostOfficeNameCompleterCalled_ShouldReturnMatchingPostOffices()
+        {
+            var postOfficefinder = new SimplePostOfficeNameCompleter();
+            var listOfMatchingOfficeNames = postOfficefinder.SuggestCompletedNames("Abusa");
+
+            Assert.That(listOfMatchingOfficeNames.First(), Is.EqualTo("Abusar B.O"));
+        }
+        
+    }
+}
